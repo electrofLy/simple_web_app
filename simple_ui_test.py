2cbf92c33ae81ea409109b48f118fbfa98befebd
Ivaylo Hristov <i.hristov@zariba.com>
2017-06-09 21:36:14 +0200

    fixed gitignore and possibly hosts

--- simple_ui_test.py
+++ simple_ui_test.py
@@ -6,11 +6,11 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 class UITest(unittest.TestCase):
 
     def setUp(self):
-        self.driver = webdriver.Remote(command_executor='http://172.18.0.35:4444/wd/hub',
+        self.driver = webdriver.Remote(command_executor='http://selenium:4444/wd/hub',
                                        desired_capabilities=DesiredCapabilities.FIREFOX)
 
     def test_index(self):
-        self.driver.get("http://172.18.0.34:8080")
+        self.driver.get("http://web:8080")
         self.assertIn("Hello World!", self.driver.page_source)
 
     def tearDown(self):
